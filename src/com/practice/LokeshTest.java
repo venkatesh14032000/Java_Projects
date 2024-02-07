a74c88d3df605346bd51b4f8087c2f0fe9f916ce
Nimmakanti Venkatesh <nimmakanti.venkatesh@eviden.com>
2024-02-07 11:53:36 +0530

    modified by venkat

--- src/com/practice/LokeshTest.java
+++ src/com/practice/LokeshTest.java
@@ -3,7 +3,7 @@ package com.practice;
 public class LokeshTest {
 
 	public static void main(String[] args) {
-		System.out.println(10+20);
+		//System.out.println(10+20);
 		System.out.println(10*20);
 
 	}
